e75ea317d4bb310eec032590d2107fdd929f1a3a
electronyoon <electronyoon@gmail.com>
2025-01-21 20:07:18 +0900

    init

---
+++ zipsoonbatch/src/main/java/com/zipsoon/zipsoonbatch/ZipsoonbatchApplication.java
@@ -0,0 +1,13 @@
+package com.zipsoon.zipsoonbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipsoonbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipsoonbatchApplication.class, args);
+	}
+
+}
---
+++ zipsoonbatch/src/test/java/com/zipsoon/zipsoonbatch/ZipsoonbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipsoon.zipsoonbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipsoonbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
