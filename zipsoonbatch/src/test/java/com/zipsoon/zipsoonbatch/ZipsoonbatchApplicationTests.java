4162608d8284a615f02117631cb667961ae65737
electronyoon <electronyoon@gmail.com>
2025-01-22 09:23:15 +0900

    test db conn, rt request

--- zipsoonbatch/src/test/java/com/zipsoon/zipsoonbatch/ZipsoonbatchApplicationTests.java
+++ zipsoonbatch/src/test/java/com/zipsoon/zipsoonbatch/ZipsoonbatchApplicationTests.java
@@ -1,13 +1,19 @@
 package com.zipsoon.zipsoonbatch;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@Slf4j
 @SpringBootTest
+@ActiveProfiles("test")
 class ZipsoonbatchApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    @DisplayName("애플리케이션 컨텍스트가 정상적으로 로드되어야 한다")
+    void contextLoads() {
+    }
 
 }
