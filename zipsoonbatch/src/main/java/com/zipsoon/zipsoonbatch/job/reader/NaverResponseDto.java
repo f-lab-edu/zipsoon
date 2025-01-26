a9ad7e2c95236cab14dd11af26614129a53a6463
electronyoon <electronyoon@gmail.com>
2025-01-26 23:15:54 +0900

    refactor: 부동산 도메인 모델과 API 응답 DTO 구조 개선

--- zipsoonbatch/src/main/java/com/zipsoon/zipsoonbatch/job/reader/NaverResponseDto.java
+++ zipsoonbatch/src/main/java/com/zipsoon/zipsoonbatch/job/reader/NaverResponseDto.java
@@ -1,11 +1,13 @@
 package com.zipsoon.zipsoonbatch.job.reader;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class NaverArticleResponseDto {
+public class NaverResponseDto {
+    @JsonProperty("isMoreData")
     private boolean isMoreData;
     private ArticleDto[] articleList;
 
@@ -24,8 +26,10 @@ public class NaverArticleResponseDto {
         private String verificationTypeCode;
         private String floorInfo;
         private String priceChangeState;
+        @JsonProperty("isPriceModification")
         private boolean isPriceModification;
         private String dealOrWarrantPrc;
+        private String rentPrc;
         private String areaName;
         private Double area1;
         private Double area2;
@@ -36,10 +40,40 @@ public class NaverArticleResponseDto {
         private String[] tagList;
         private String buildingName;
         private Integer sameAddrCnt;
+        private Integer sameAddrDirectCnt;
         private String sameAddrMaxPrc;
         private String sameAddrMinPrc;
+        private String cpid;
+        private String cpName;
+        private String cpPcArticleUrl;
+        private String cpPcArticleBridgeUrl;
+        @JsonProperty("cpPcArticleLinkUseAtArticleTitleYn")
+        private Boolean cpPcArticleLinkUseAtArticleTitleYn;
+        @JsonProperty("cpPcArticleLinkUseAtCpNameYn")
+        private Boolean cpPcArticleLinkUseAtCpNameYn;
+        private String cpMobileArticleUrl;
+        @JsonProperty("cpMobileArticleLinkUseAtArticleTitleYn")
+        private Boolean cpMobileArticleLinkUseAtArticleTitleYn;
+        @JsonProperty("cpMobileArticleLinkUseAtCpNameYn")
+        private Boolean cpMobileArticleLinkUseAtCpNameYn;
         private String latitude;
         private String longitude;
         private String realtorName;
+        private String realtorId;
+        @JsonProperty("tradeCheckedByOwner")
+        private Boolean tradeCheckedByOwner;
+        @JsonProperty("isDirectTrade")
+        private Boolean isDirectTrade;
+        @JsonProperty("isInterest")
+        private Boolean isInterest;
+        @JsonProperty("isLocationShow")
+        private Boolean isLocationShow;
+        @JsonProperty("isComplex")
+        private Boolean isComplex;
+        private String detailAddress;
+        private String detailAddressYn;
+        private String virtualAddressYn;
+        @JsonProperty("isVrExposed")
+        private Boolean isVrExposed;
     }
 }
