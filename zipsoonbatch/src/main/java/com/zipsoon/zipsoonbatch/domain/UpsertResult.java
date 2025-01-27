fb6c9f61758749a7ad167cb81abea9c3cfea9271
electronyoon <electronyoon@gmail.com>
2025-01-27 16:56:55 +0900

    feat. 매물 정보 upsert 결과 도메인 구현

--- zipsoonbatch/src/main/java/com/zipsoon/zipsoonbatch/domain/UpsertResult.java
+++ zipsoonbatch/src/main/java/com/zipsoon/zipsoonbatch/domain/UpsertResult.java
@@ -1,2 +1,21 @@
-package com.zipsoon.zipsoonbatch.domain;public class UpsertResult {
+package com.zipsoon.zipsoonbatch.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpsertResult {
+    private Long id;
+    private String operation;   // INSERT or UPDATE
+
+    public boolean isUpdate() {
+        return "UPDATE".equals(operation);
+    }
+
+    public boolean isInsert() {
+        return "INSERT".equals(operation);
+    }
 }
