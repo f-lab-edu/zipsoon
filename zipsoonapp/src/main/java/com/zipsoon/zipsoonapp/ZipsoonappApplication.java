876206367d39f960d4ef8d8e872830e680f5731e
electronyoon <electronyoon@gmail.com>
2025-01-29 13:31:04 +0900

    init

---
+++ zipsoonapp/src/main/java/com/zipsoon/zipsoonapp/ZipsoonappApplication.java
@@ -0,0 +1,13 @@
+package com.zipsoon.zipsoonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipsoonappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipsoonappApplication.class, args);
+	}
+
+}
