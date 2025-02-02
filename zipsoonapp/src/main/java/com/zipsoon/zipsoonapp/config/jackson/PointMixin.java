9043bf4c039f367d75bdbe85a62b5ab38cba148d
electronyoon <electronyoon@gmail.com>
2025-02-02 21:53:28 +0900

    Add untracked and deleted files

--- zipsoonapp/src/main/java/com/zipsoon/zipsoonapp/config/jackson/PointMixin.java
+++
@@ -1,7 +0,0 @@
-package com.zipsoon.zipsoonapp.config.jackson;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties({"firstPoint", "lastPoint"})
-public abstract class PointMixin {
-}
--- zipsoonbatch/src/main/java/com/zipsoon/zipsoonbatch/repository/mapper/PropertyHistoryMapper.java
+++
@@ -1,9 +0,0 @@
-package com.zipsoon.zipsoonbatch.repository.mapper;
-
-import com.zipsoon.zipsoonbatch.domain.PropertyHistory;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PropertyHistoryMapper {
-    void save(PropertyHistory propertyHistory);
-}
