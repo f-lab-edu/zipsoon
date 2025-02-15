4b40e613422439b153fd78777c884c5b6eec8790
electronyoon <electronyoon@gmail.com>
2025-02-16 03:31:10 +0900

    #40 feat. Jackson 설정 추가 및 Security 설정 개선

---
+++ api/src/main/java/com/zipsoon/api/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.zipsoon.api.config;
+
+import com.zipsoon.api.security.filter.JwtAuthenticationFilter;
+import com.zipsoon.api.security.handler.JwtAuthenticationEntryPoint;
+import com.zipsoon.api.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtProvider jwtProvider;
+
+	@Bean
+	public WebSecurityCustomizer configure() {
+		return web -> web.ignoring()
+				.requestMatchers(
+                    "/swagger-ui.html",
+                    "/swagger-ui/**",
+                    "/swagger-resources/**",
+                    "/v3/api-docs/**",
+                    "/webjars/**",
+                    "/error"
+		        );
+	}
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(
+                new JwtAuthenticationFilter(jwtProvider),
+                UsernamePasswordAuthenticationFilter.class
+            )
+            .exceptionHandling(ex ->
+                ex.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+            )
+            .build();
+    }
+
+}
