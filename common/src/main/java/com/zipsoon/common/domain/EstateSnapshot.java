9d1327771bcd4a87cac049239e4175a1a1830277
electronyoon <electronyoon@gmail.com>
2025-03-03 00:46:42 +0900

    #58 refactor. 도메인 모델 분리 및 스냅샷 패턴 적용

--- common/src/main/java/com/zipsoon/common/domain/EstateSnapshot.java
+++ common/src/main/java/com/zipsoon/common/domain/EstateSnapshot.java
@@ -1,6 +1,5 @@
 package com.zipsoon.common.domain;
 
-import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.*;
 import org.locationtech.jts.geom.Geometry;
@@ -48,97 +47,4 @@ public class EstateSnapshot {
     private LocalDateTime createdAt;
 
     private List<String> imageUrls;
-
-    public enum PlatformType {
-        네이버("네이버"),
-        직방("직방");
-
-        private final String koreanName;
-
-        PlatformType(String koreanName) {
-            this.koreanName = koreanName;
-        }
-
-        public String getKoreanName() {
-            return koreanName;
-        }
-
-        public static PlatformType of(String koreanName) {
-            for (PlatformType platformType: values()) {
-                if (platformType.koreanName.equals(koreanName)) {
-                    return platformType;
-                }
-            }
-            return null;
-        }
-    }
-
-    public enum EstateType {
-        APT("아파트"),
-        OPST("오피스텔"),
-        VL("빌라"),
-        ABYG("아파트분양권"),
-        OBYG("오피스텔분양권"),
-        JGC("재건축"),
-        JWJT("전원주택"),
-        DDDGG("단독/다가구"),
-        SGJT("상가주택"),
-        HOJT("한옥주택"),
-        JGB("재개발"),
-        OR("원룸"),
-        SG("상가"),
-        SMS("사무실"),
-        GJCG("공장/창고"),
-        GM("건물"),
-        TJ("토지"),
-        APTHGJ("지식산업센터");
-
-        private final String koreanName;
-
-        EstateType(String koreanName) {
-            this.koreanName = koreanName;
-        }
-
-        @JsonValue
-        public String getKoreanName() {
-            return koreanName;
-        }
-
-        public static EstateType of(String koreanName) {
-            for (EstateType estateType : values()) {
-                if (estateType.koreanName.equals(koreanName)) {
-                    return estateType;
-                }
-            }
-            return null;
-        }
-    }
-
-    public enum TradeType {
-        A1("매매"),
-        B1("전세"),
-        B2("월세"),
-        B3("단기임대");
-
-        private final String koreanName;
-
-        TradeType(String koreanName) {
-            this.koreanName = koreanName;
-        }
-
-        @JsonValue
-        public String getKoreanName() {
-            return koreanName;
-        }
-
-        public static TradeType of(String koreanName) {
-            for (TradeType tradeType: values()) {
-                if (tradeType.koreanName.equals(koreanName)) {
-                    return tradeType;
-                }
-            }
-            return null;
-        }
-    }
-
 }
