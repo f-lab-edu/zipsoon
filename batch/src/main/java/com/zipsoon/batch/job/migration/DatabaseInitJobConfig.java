41fab6e69b5e47852746983973bebe0d82c17836
electronyoon <electronyoon@gmail.com>
2025-03-05 01:02:56 +0900

    fix. 초기 작업에 법정동코드 테이블 초기화 step 추가

--- batch/src/main/java/com/zipsoon/batch/job/migration/DatabaseInitJobConfig.java
+++ batch/src/main/java/com/zipsoon/batch/job/migration/DatabaseInitJobConfig.java
@@ -1,5 +1,6 @@
 package com.zipsoon.batch.job.migration;
 
+import com.zipsoon.batch.job.migration.tasklet.DatabaseDongcodesInitTasklet;
 import com.zipsoon.batch.job.migration.tasklet.DatabaseMigrationTasklet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,23 +14,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 /**
- * 데이터베이스 스냅샷 이동 및 비우기 작업 설정
+ * 데이터베이스 초기화 작업 설정
  * 이 작업은 다른 모든 작업 전에 실행되어야 함
+ * 1. 법정동코드 테이블 초기화 (dongcodesInitStep)
+ * 2. 매물 데이터 스냅샷 이동 및 비우기 (migrationStep)
  */
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
-public class DatabaseMigrationJobConfig {
-    private static final String JOB_NAME = "databaseMigrationJob";
+public class DatabaseInitJobConfig {
+    private static final String JOB_NAME = "databaseInitJob";
 
     private final JobRepository jobRepository;
     private final PlatformTransactionManager transactionManager;
     private final DatabaseMigrationTasklet databaseMigrationTasklet;
+    private final DatabaseDongcodesInitTasklet databaseDongcodesInitTasklet;
 
     @Bean(name = JOB_NAME)
-    public Job databaseMigrationJob() {
+    public Job databaseInitJob() {
         return new JobBuilder(JOB_NAME, jobRepository)
-            .start(databaseMigrationStep())
+            .start(dongcodesInitStep())
+            .next(databaseMigrationStep())
+            .build();
+    }
+    
+    @Bean
+    public Step dongcodesInitStep() {
+        return new StepBuilder("dongcodesInitStep", jobRepository)
+            .tasklet(databaseDongcodesInitTasklet, transactionManager)
             .build();
     }
     
